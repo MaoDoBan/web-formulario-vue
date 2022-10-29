605cf2be7c430f9cc4e2bf80662b1113357fd051
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-10-29 10:29:23 -0300

    duas formas de checkbox

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -11,10 +11,30 @@
       <option value="webdev">Desenvolvedor Web</option>
       <option value="webdedigner">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="termos">
+      <label>Aceito os termos de uso</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="luigi" v-model="nomes">
+      <label>Luigi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="mario" v-model="nomes">
+      <label>Mario</label>
+    </div>
+    <div>
+      <input type="checkbox" value="ricardo" v-model="nomes">
+      <label>Ricardo</label>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Cargo: {{ cargo }}</p>
+  <p>Termos: {{ termos }}</p>
+  <p>Nomes: {{ nomes }}</p>
 </template>
 
 <script>
@@ -23,7 +43,9 @@ export default {
     return {
       email: "",
       password: "",
-      cargo: "webdev"
+      cargo: "webdev",
+      termos: false,
+      nomes: []
     };
   }
 }
@@ -55,5 +77,12 @@ input, select{
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"]{
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
